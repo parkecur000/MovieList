e6b4627c02f132ae17adb5ede0d2f49d6bb178f7
Curtis <curtmparker@gmail.com>
2021-02-03 18:01:07 -0700

    Add project files.

---
+++ MovieList/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieList.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult NewMovies()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewMovies(Movies appResponse)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                TempStorage.AddMovie(appResponse); //Adds the movie to the movies list by calling the AddMovie method
+                return View("Confirmation", appResponse);
+            }
+        }
+        public IActionResult NewMoviesDisplay()
+        {
+            return View(TempStorage.Movies);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieList/Models/Movies.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieList.Models
+{
+    public class Movies
+    {
+        [Required]
+        public string category { get; set; }
+        [Required]
+        public string title { get; set; }
+        [Required]
+        public string year { get; set; }
+        [Required]
+        public string director { get; set; }
+        [Required]
+        public string rating { get; set; }
+        public bool? edited { get; set; }
+        public string lent_to { get; set; }
+        public string notes { get; set; }
+
+
+    }
+}
